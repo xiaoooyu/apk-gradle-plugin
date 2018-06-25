fc3f097a29f3b0513e41662ec3c6946149f240b1
Yin Xiaoyu <yin.xiaoyu@foxmail.com>
2018-06-25 11:44:41 +0800

    abstract common attributes into AndroidManifestAwareness

--- src/main/groovy/AndroidManifestReader.groovy
+++ src/main/groovy/AndroidManifestReader.groovy
@@ -1,43 +1,22 @@
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-class AndroidManifestReader {
+class AndroidManifestReader extends AndroidManifestAwareness {
 
-    File inputFile
     InputStream inputStream
-
-    static def MAGIC_NUMBER = 0x00080003
-    int totalSize = 0
-
-    static def RESOURCE_ID_CHUNK = 0x00080180
-
-    static def STRING_CHUNK = 0x001C0001
-    int stringChunkSize = 0
-    int stringCount = 0
-    int styleCount = 0
-    int unknown = 0
-    int stringPoolOffset = 0
-    int stylePoolOffset = 0
-    int[] stringOffsets
-    int[] styleOffsets
-    List<String> stringPool
-
-    byte[] others
 
     InputStream getInputStream() {
         if (inputStream == null) {
-            inputStream = new FileInputStream(inputFile)
+            inputStream = new FileInputStream(filePath)
         }
         return inputStream
     }
 
     AndroidManifestReader(String theFilePath) {
-        inputFile = new File(theFilePath)
+        super(theFilePath)
     }
 
     void read() {
-//        printAllBytes()
-
         if (MAGIC_NUMBER != readInt()) {
             return
         }
@@ -71,10 +50,6 @@ class AndroidManifestReader {
                 def toAdd = readString()
                 stringPool.add(toAdd)
             }
-
-//            stringPool.each {
-//                println it
-//            }
         }
 
         others = readOthers()
@@ -112,64 +87,11 @@ class AndroidManifestReader {
             outputStream.write(buff, 0, read)
         }
         others = outputStream.toByteArray()
-        print(others)
-    }
-
-    void printAllBytes() {
-        byte[] allByte = inputFile.readBytes()
-        println "Have read ${allByte.length} bytes totally"
-
-        def pos = 0
-        byte[] fourBytes = new byte[4]
-        allByte.each {
-            fourBytes[pos % 4] = allByte[pos]
-            pos ++
-            if (pos % 4 == 0) {
-                print(fourBytes)
-                print " "
-            }
-            if ((pos % 16) == 0) println ""
-        }
-
-//
-//        ByteBuffer headerByteBuffer = new By
     }
 
     byte[] read(int len) {
         byte[] readBytes = new byte[len]
         getInputStream().read(readBytes)
         return readBytes
-    }
-
-    int toInt(byte[] source) {
-        ByteBuffer buffer = ByteBuffer.wrap(source)
-        buffer.order(ByteOrder.LITTLE_ENDIAN)
-        return buffer.getInt()
-    }
-
-    short toShort(byte[] source) {
-        ByteBuffer buffer = ByteBuffer.wrap(source)
-        buffer.order(ByteOrder.LITTLE_ENDIAN)
-        return buffer.getShort()
-    }
-
-    void print(byte[] bytes, boolean reverse = false) {
-        def range = !reverse ? (0..bytes.length - 1) : (bytes.length - 1 .. 0)
-
-        range.each{
-            if (it % 16 == 0) println ""
-            print String.format("%02X ", bytes[it])
-        }
-    }
-
-    byte[] reverse(byte[] source) {
-        int len = source.length
-        (0..(len-1)/2).each {
-            def exchangeIndex = len - 1 - it
-            def tmp = source[it]
-            source[it] = source[exchangeIndex]
-            source[exchangeIndex] = tmp
-        }
-        return source
     }
 }
