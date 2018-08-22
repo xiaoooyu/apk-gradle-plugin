91a5bdc2620caf3bd9488ded0a7f39e053ffee91
guodong <guodong@teambition.com>
2018-08-22 12:50:12 +0800

    OPT make channel work work

--- src/main/kotlin/com/github/xiaoooyu/gradle/tasks/ChannelWork.kt
+++ src/main/kotlin/com/github/xiaoooyu/gradle/tasks/ChannelWork.kt
@@ -2,45 +2,46 @@ package com.github.xiaoooyu.gradle.tasks
 
 import com.github.xiaoooyu.android.AndroidManifestReader
 import com.github.xiaoooyu.android.AndroidManifestWriter
-import com.github.xiaoooyu.android.*
+import com.github.xiaoooyu.android.extractZip
+import com.github.xiaoooyu.android.replaceZip
 import com.github.xiaoooyu.gradle.ApkExtension
-
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
-import org.gradle.api.logging.Logger
 import org.gradle.api.tasks.TaskAction
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
-open class ChannelWork: DefaultTask() {
-    var apks: FileCollection? = null
-    var placeHolder: String? = "channel_placeholder"
-    var channels: Array<String>? = emptyArray()
-    var to: String = "build/archives"
-    var env: ApkExtension? = null
-
+open class ChannelWork : DefaultTask() {
     companion object {
         const val fileName = "AndroidManifest.xml"
     }
 
+    var apks: FileCollection? = null
+    var placeHolder: String? = "channel_placeholder"
+    var channels: Array<String>? = emptyArray()
+    var env: ApkExtension? = null
+    var to: String = "build/archives"
+    var debug: Boolean = false
+
     @TaskAction
     fun perform() {
-        to.let {
-            project.file(to).mkdirs()
-        }
-
+        project.file(to).mkdirs()
+        project.file(getTemporaryApkPath()).mkdirs()
         apks?.files?.forEach {
             it?.let {
                 batchReplaceString(it, channels)
             }
         }
-
-        project.fileTree(to).files.forEach(::signApk)
+        project.fileTree(getTemporaryApkPath()).files.forEach { apk ->
+            signApk(apk, project.file(to).absolutePath)
+        }
     }
 
-    internal fun batchReplaceString(apk: File, channels: Array<String>?) {
-        val oriFile = project.file("${to}/AndroidManifest_ori.xml")
+    private fun batchReplaceString(apk: File, channels: Array<String>?) {
+        val oriFile = project.file("${getTemporaryApkPath()}/AndroidManifest_ori.xml")
         apk.extractZip(fileName, oriFile.absolutePath)
 
         val reader = AndroidManifestReader(oriFile.absolutePath)
@@ -67,8 +68,8 @@ open class ChannelWork: DefaultTask() {
         oriFile.delete()
     }
 
-    internal fun replaceString(apk: File, channelName: String, reader: AndroidManifestReader, index: Int) {
-        val newFile = project.file("${to}/AndroidManifest_${channelName}.xml").apply {
+    private fun replaceString(apk: File, channelName: String, reader: AndroidManifestReader, index: Int) {
+        val newFile = project.file("${getTemporaryApkPath()}/AndroidManifest_$channelName.xml").apply {
             File(parent).mkdirs()
         }
 
@@ -81,7 +82,7 @@ open class ChannelWork: DefaultTask() {
         }
 
         // copy template apk file
-        val targetFile = project.file("${to}/${apk.nameWithoutExtension}-${channelName}.${apk.extension}")
+        val targetFile = project.file("${getTemporaryApkPath()}/${apk.nameWithoutExtension}-$channelName.${apk.extension}")
         Files.copy(apk.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
 
         targetFile.replaceZip(newFile.absolutePath, fileName)
@@ -89,7 +90,7 @@ open class ChannelWork: DefaultTask() {
         newFile.delete()
     }
 
-    internal fun signApk(apk: File) {
+    private fun signApk(originApk: File, outputPath: String) {
         val runtime = Runtime.getRuntime()
         val cmdAlign = env?.zipalignExec ?: "zipalign"
         val cmdSign = env?.apksignerExec ?: "apksigner"
@@ -97,22 +98,24 @@ open class ChannelWork: DefaultTask() {
         val keyAlias = env?.keyAlias ?: ""
         val storePass = env?.storePass ?: ""
         val keyPass = env?.keyPass ?: ""
-        val filePath = apk.absolutePath
+        val filePath = originApk.absolutePath
 
-        TODO("change output zipfile name")
-        val alignProc = runtime.exec("${cmdAlign} -f 4 $filePath $filePath")
-        if (alignProc.waitFor() == 0) {
-            logger.info("$filePath zipalign success")
-        } else {
-            logger.error("$filePath zipalign error")
-        }
+        val alignCommand = "$cmdAlign -f 4 $filePath $outputPath/${originApk.name}"
+        println("align command: $alignCommand")
+        val alignProc = runtime.exec(alignCommand)
 
+        BufferedReader(InputStreamReader(alignProc.errorStream)).forEachLine { if (debug) println("$cmdAlign/E: $it") }
+        BufferedReader(InputStreamReader(alignProc.inputStream)).forEachLine { if (debug) println("$cmdAlign/I: $it") }
 
-        val signProc = runtime.exec("${cmdSign} sign --ks $keyStore --ks-key-alias $keyAlias --ks-pass pass:$storePass --key-pass pass:$keyPass ${filePath}")
-        if (signProc.waitFor() == 0) {
-            logger.info("$filePath re-sign success")
-        } else {
-            logger.error("$filePath re-sign error")
-        }
+        val signCommand = "$cmdSign sign --ks $keyStore --ks-key-alias $keyAlias --ks-pass pass:$storePass --key-pass pass:$keyPass $outputPath/${originApk.name}"
+        println("signCommand: $signCommand")
+        val signProc = runtime.exec(signCommand)
+
+        BufferedReader(InputStreamReader(signProc.errorStream)).forEachLine { if (debug) println("$cmdSign/E: $it") }
+        BufferedReader(InputStreamReader(signProc.inputStream)).forEachLine { if (debug) println("$cmdSign/I: $it") }
+
+        originApk.delete()
     }
+
+    private fun getTemporaryApkPath() = "$to/temp"
 }
